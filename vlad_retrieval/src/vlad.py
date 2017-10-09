93d61a19bc5b8cdcbb8014b08acdaf10cbbe6229
willard-yuan <yongyuanstu@gmail.com>
2017-10-09 22:31:14 +0800

    vlad intra-normalization

--- vlad_retrieval/src/vlad.py
+++ vlad_retrieval/src/vlad.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Anthor: yongyuan.name
 
 import os,h5py
 import numpy as np
 from yael import ynumpy
 
-
+# save feature to h5py
 h5f = h5py.File('./models/vlad_128_8192.h5', 'w')
+# load PCA and KMeans model
 centroids = np.load('./models/centroids_data.npy')
 pca_transform = np.load('./models/pca_data.npy')
 mean = np.load('./models/mean_data.npy')
 
-# 
+#
 txt_path = '/home/yuanyong/py/fv_retrieval/oxford.txt'
 with open(txt_path, 'r') as f:
     content = f.readlines()
     content = [x.strip() for x in content]
 
-# 
+# compute VLAD feature
 features = []
 image_names = []
 for i, line in enumerate(content):
@@ -52,30 +54,26 @@ for i, line in enumerate(content):
     image_desc = np.dot(image_desc - mean, pca_transform)
     image_desc = image_desc.astype(np.float32)
 
-    # compute the Fisher vector, using only the derivative w.r.t mu
-
-    k = centroids.shape[0]     # nb of centroids
-    d = centroids.shape[1]     # local descriptor dimension
-    n = image_desc.shape[0]    # nb of local image descriptors
-    vlad = np.zeros(k*d, dtype = np.float32)   # global VLAD descriptor for the image (output) 
-    ynumpy.yael.vlad_compute(k, d, ynumpy.yael.numpy_to_fvec_ref(centroids), n, ynumpy.yael.numpy_to_fvec_ref(image_desc), ynumpy.yael.numpy_to_fvec_ref(vlad))
+    # compute VLAD
+    vlad = ynumpy.vlad(centroids, image_desc)  # num_centroids * d
+    # L2 normalization
+    norms_vlad = np.sqrt(np.sum(vlad ** 2, 1))
+    norms_vlad[norms_vlad < 1e-12] = 1e-12
+    vlad /= norms_vlad.reshape(-1, 1)
+    vlad = vlad.flatten()
     features.append(vlad)
     image_names.append(img_name)
 
 # make one matrix with all FVs
 features = np.vstack(features)
 
-# normalizations are done on all descriptors at once
-
 # power-normalization, square-rooting normalization
 features = np.sign(features) * np.abs(features) ** 0.5
 
 # L2 normalize
 norms = np.sqrt(np.sum(features ** 2, 1))
+norms[norms < 1e-12] = 1e-12
 features /= norms.reshape(-1, 1)
-
-# L2 normalization
-#features = features/np.sqrt(np.dot(features, features))
 
 # save feats
 print "number samples: %d, dimension: %d, number names: %d" %(features.shape[0], features.shape[1], len(image_names))
